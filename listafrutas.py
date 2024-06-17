23327c86f8d0f94293e0bf432c45550be22de871
Keta23199 <165030072+Keta23199@users.noreply.github.com>
2024-06-17 08:56:33 -0300

    Create listafrutas.py

---
+++ listafrutas.py
@@ -0,0 +1,10 @@
+listafrutas = ["banana","morango","limão"]
+fruta = "banana"
+print(listafrutas[2])
+print(fruta[0])
+print(listafrutas[2][0])
+listafrutas.append("abacaxi")
+print(listafrutas)
+listafrutas.remove("mamão")
+print(listafrutas)
+listafrutas.sort()
